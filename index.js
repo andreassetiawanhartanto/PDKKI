1feb9980f85759a3e7aa46a09068d1a075cf3805
Andreas Setiawan Hartanto <andreasset2@gmail.com>
2016-01-10 22:06:58 +1100

    add sticky header

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var helloApp = angular.module("helloApp", []);
-	helloApp.controller("HelloCtrl", function($scope) {
+	helloApp.controller("MainCtrl", function($scope) {
 		$scope.name = "Calvin Hobbes";
+		$scope.selected = "home";
+
+		 $scope.select= function(item) {
+        	$scope.selected = item;
+		 };
+
+		 $scope.isActive = function(item) {
+		    return $scope.selected === item;
+		 };	
+
 	});
