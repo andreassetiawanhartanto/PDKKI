ee902c064e71dc7ab496ace6538f1e6c47cfb47a
Andreas Hartanto <ahartanto@ajfpartnership.com.au>
2016-01-15 16:46:00 +1100

    update content

---
+++ bower_components/angular-ui/ui-bootstrap-custom-1.0.3.js
@@ -0,0 +1,173 @@
+/*
+* angular-ui-bootstrap
+* http://angular-ui.github.io/bootstrap/
+
+* Version: 1.0.3 - 2016-01-11
+* License: MIT
+*/angular.module("ui.bootstrap", ["ui.bootstrap.tabs"]);
+angular.module('ui.bootstrap.tabs', [])
+
+.controller('UibTabsetController', ['$scope', function ($scope) {
+  var ctrl = this,
+      tabs = ctrl.tabs = $scope.tabs = [];
+
+  ctrl.select = function(selectedTab) {
+    angular.forEach(tabs, function(tab) {
+      if (tab.active && tab !== selectedTab) {
+        tab.active = false;
+        tab.onDeselect();
+        selectedTab.selectCalled = false;
+      }
+    });
+    selectedTab.active = true;
+    // only call select if it has not already been called
+    if (!selectedTab.selectCalled) {
+      selectedTab.onSelect();
+      selectedTab.selectCalled = true;
+    }
+  };
+
+  ctrl.addTab = function addTab(tab) {
+    tabs.push(tab);
+    // we can't run the select function on the first tab
+    // since that would select it twice
+    if (tabs.length === 1 && tab.active !== false) {
+      tab.active = true;
+    } else if (tab.active) {
+      ctrl.select(tab);
+    } else {
+      tab.active = false;
+    }
+  };
+
+  ctrl.removeTab = function removeTab(tab) {
+    var index = tabs.indexOf(tab);
+    //Select a new tab if the tab to be removed is selected and not destroyed
+    if (tab.active && tabs.length > 1 && !destroyed) {
+      //If this is the last tab, select the previous tab. else, the next tab.
+      var newActiveIndex = index === tabs.length - 1 ? index - 1 : index + 1;
+      ctrl.select(tabs[newActiveIndex]);
+    }
+    tabs.splice(index, 1);
+  };
+
+  var destroyed;
+  $scope.$on('$destroy', function() {
+    destroyed = true;
+  });
+}])
+
+.directive('uibTabset', function() {
+  return {
+    transclude: true,
+    replace: true,
+    scope: {
+      type: '@'
+    },
+    controller: 'UibTabsetController',
+    templateUrl: 'uib/template/tabs/tabset.html',
+    link: function(scope, element, attrs) {
+      scope.vertical = angular.isDefined(attrs.vertical) ? scope.$parent.$eval(attrs.vertical) : false;
+      scope.justified = angular.isDefined(attrs.justified) ? scope.$parent.$eval(attrs.justified) : false;
+    }
+  };
+})
+
+.directive('uibTab', ['$parse', function($parse) {
+  return {
+    require: '^uibTabset',
+    replace: true,
+    templateUrl: 'uib/template/tabs/tab.html',
+    transclude: true,
+    scope: {
+      active: '=?',
+      heading: '@',
+      onSelect: '&select', //This callback is called in contentHeadingTransclude
+                          //once it inserts the tab's content into the dom
+      onDeselect: '&deselect'
+    },
+    controller: function() {
+      //Empty controller so other directives can require being 'under' a tab
+    },
+    controllerAs: 'tab',
+    link: function(scope, elm, attrs, tabsetCtrl, transclude) {
+      scope.$watch('active', function(active) {
+        if (active) {
+          tabsetCtrl.select(scope);
+        }
+      });
+
+      scope.disabled = false;
+      if (attrs.disable) {
+        scope.$parent.$watch($parse(attrs.disable), function(value) {
+          scope.disabled = !! value;
+        });
+      }
+
+      scope.select = function() {
+        if (!scope.disabled) {
+          scope.active = true;
+        }
+      };
+
+      tabsetCtrl.addTab(scope);
+      scope.$on('$destroy', function() {
+        tabsetCtrl.removeTab(scope);
+      });
+
+      //We need to transclude later, once the content container is ready.
+      //when this link happens, we're inside a tab heading.
+      scope.$transcludeFn = transclude;
+    }
+  };
+}])
+
+.directive('uibTabHeadingTransclude', function() {
+  return {
+    restrict: 'A',
+    require: '^uibTab',
+    link: function(scope, elm) {
+      scope.$watch('headingElement', function updateHeadingElement(heading) {
+        if (heading) {
+          elm.html('');
+          elm.append(heading);
+        }
+      });
+    }
+  };
+})
+
+.directive('uibTabContentTransclude', function() {
+  return {
+    restrict: 'A',
+    require: '^uibTabset',
+    link: function(scope, elm, attrs) {
+      var tab = scope.$eval(attrs.uibTabContentTransclude);
+
+      //Now our tab is ready to be transcluded: both the tab heading area
+      //and the tab content area are loaded.  Transclude 'em both.
+      tab.$transcludeFn(tab.$parent, function(contents) {
+        angular.forEach(contents, function(node) {
+          if (isTabHeading(node)) {
+            //Let tabHeadingTransclude know.
+            tab.headingElement = node;
+          } else {
+            elm.append(node);
+          }
+        });
+      });
+    }
+  };
+
+  function isTabHeading(node) {
+    return node.tagName && (
+      node.hasAttribute('uib-tab-heading') ||
+      node.hasAttribute('data-uib-tab-heading') ||
+      node.hasAttribute('x-uib-tab-heading') ||
+      node.tagName.toLowerCase() === 'uib-tab-heading' ||
+      node.tagName.toLowerCase() === 'data-uib-tab-heading' ||
+      node.tagName.toLowerCase() === 'x-uib-tab-heading'
+    );
+  }
+});
+angular.module('ui.bootstrap.tabs').run(function() {!angular.$$csp().noInlineStyle && angular.element(document).find('head').prepend('<style type="text/css">.uib-tab > div{position:relative;display:block;padding:10px 15px;outline:0;color:#337ab7;}.uib-tab > div:focus,.uib-tab > div:hover{background-color:#eee;color:#23527c;}.uib-tab.disabled > div{color:#777;}.uib-tab.disabled > div:focus,.uib-tab.disabled > div:hover{color:#777;cursor:not-allowed;background-color:transparent;}.nav-tabs > .uib-tab > div{margin-right:2px;line-height:1.42857143;border:1px solid transparent;border-radius:4px 4px 0 0;}.nav-tabs > .uib-tab > div:hover{border-color:#eee #eee #ddd;}.nav-tabs > .uib-tab.active > div,.nav-tabs > .uib-tab.active > div:focus,.nav-tabs > .uib-tab.active > div:hover{color:#555;cursor:default;background-color:#fff;border-color:#ddd #ddd transparent #ddd;}.nav-pills > .uib-tab > div{border-radius:4px;}.nav-pills > .uib-tab.active > div,.nav-pills > .uib-tab.active > div:focus,.nav-pills > .uib-tab.active > div:hover{color:#fff;background-color:#337ab7;}</style>'); });
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var helloApp = angular.module("helloApp", []);
-	helloApp.controller("MainCtrl",["$scope","$sce",function($scope,$sce) {
+var helloApp = angular.module("helloApp", ['ui.bootstrap']);
+	helloApp.controller("MainCtrl",["$scope","$sce","$http",function($scope,$sce,$http) {
 		$scope.name = "Calvin Hobbes";
 		$scope.selected = "home";
 
@@ -45,10 +45,40 @@ var helloApp = angular.module("helloApp", []);
 			 	"name" : "Retreat KADO 9-11 Oct 2015",
 				"url" : "https://www.youtube.com/embed/VgsSjRPJoCM?autoplay=1",
 				"thumbnail" : "http://img.youtube.com/vi/VgsSjRPJoCM/2.jpg",
+		 },{
+			 	"name" : "Retret K.A.D.O 2015",
+				"url" : "https://www.youtube.com/embed/URKEhHCRoxk?autoplay=1",
+				"thumbnail" : "http://img.youtube.com/vi/URKEhHCRoxk/2.jpg",
+		 },{
+			 	"name" : "PDKKI Melbourne: A Snapshot",
+				"url" : "https://www.youtube.com/embed/HYlPmc3WiPc?autoplay=1",
+				"thumbnail" : "http://img.youtube.com/vi/HYlPmc3WiPc/2.jpg",
+		 },{
+			 	"name" : "Retreat 2012 Teaser",
+				"url" : "https://www.youtube.com/embed/hc4PPI1g3pQ?autoplay=1",
+				"thumbnail" : "http://img.youtube.com/vi/hc4PPI1g3pQ/2.jpg",
+		 },{
+			 	"name" : "PDKKI Melbourne Choir",
+				"url" : "https://www.youtube.com/embed/ioDJj4KbBi0?autoplay=1",
+				"thumbnail" : "http://img.youtube.com/vi/ioDJj4KbBi0/2.jpg",
 		 }];
 
 		 $scope.changeVideoSrc = function(video){
 			 $scope.currentUrl = $sce.trustAsResourceUrl(video.url);
 		 }
 
+		//  setTimeout(function(){
+		// 			 FB.api(
+		// 				 '/me/',
+		// 				 'GET',
+		// 				 {"fields":"albums.limit(5){photos.limit(10)}","access_token":"CAACEdEose0cBAOeXMT2bkoNiRomWolEktR6581KXgD04JY65sK4QJII3hcjHbiRhTuxrMsZAHd2AM1C7knZC3uSaCVsZBYLA9N4W9GJXyRsjMTBH44utOZC7Xh0lOhr8PD8oe0ir3yrNdxUFbZCst5NFVdZC7y9t2bGsTO7mwNW7mjcgVpEMcJz3lYbBPAXxVGuCTHvAAKiAZDZD"},
+		// 				 function(response) {
+		// 					 	 $scope.albums = angular.copy(response.albums.data);
+		// 						 console.log($scope.albums);
+		// 						 $scope.$apply();
+		// 				 }
+		// 		 );
+		// 	}, 3000);
 	}]);
+
+	// https://developers.facebook.com/tools/explorer/
